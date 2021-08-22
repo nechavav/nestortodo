9f70c776f86bf187dca6e689e5db355dee3ed211
nechavav <nestorechavarria10@gmail.com>
2021-08-22 17:46:01 -0500

    Unit Test Preparation

---
+++ nestortodo.Test/Helpers/NullScope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nestortodo.Test.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose() { }
+
+        private NullScope() { }
+
+    }
+}
